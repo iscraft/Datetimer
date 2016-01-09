e07855618736f3669ad12e82f62c5543f8fb6ed4
is <moaphi@gmail.com>
2016-01-09 23:22:16 +0800

    v0.1

---
+++ datetime.js
@@ -0,0 +1,189 @@
+function getDaysInMonth(year,month){
+	month		= parseInt(month,10);
+	var temp	= new Date(year,month,0);
+	return temp.getDate();
+}
+
+function getdayscontent(day_number){
+	var day_content	= '<li data-sn="01">01</li>';
+	for(var i=2;i<=day_number;i++){
+		i = i<10?'0'+i:i;
+		day_content+=("<li data-sn="+i+">"+i+"</li>");
+	}
+	return day_content;
+}
+
+function default_select(){
+	if (year_tmp==undefined){
+		var year_e=$('#year li[data-sn='+year+']');
+		$(year_e).addClass('select');
+		year_tmp=year;
+	}else{
+		var year_e=$('#year li[data-sn='+year_tmp+']');
+		$(year_e).addClass('select');
+	}
+	if (month_tmp==undefined){
+		var month_e=$('#month li[data-sn='+month+']');
+		$(month_e).addClass('select');
+		month_tmp=month;
+	}else{
+		var month_e=$('#month li[data-sn='+month_tmp+']');
+		$(month_e).addClass('select');
+	}
+	if (day_tmp==undefined){
+		var day_e=$('#day li[data-sn='+day+']');
+		$(day_e).addClass('select');
+		day_tmp=day;
+	}else{
+		var day_e=$('#day li[data-sn='+day_tmp+']');
+		$(day_e).addClass('select');
+	}
+	if (hour_tmp==undefined){
+		var hour_e=$('#hour li[data-sn='+hour+']');
+		$(hour_e).addClass('select');
+		hour_tmp=hour;
+	}else{
+		var hour_e=$('#hour li[data-sn='+hour_tmp+']');
+		$(hour_e).addClass('select');
+	}
+	if (min_tmp==undefined){
+		var min_e=$('#min li[data-sn='+min+']');
+		$(min_e).addClass('select');
+		min_tmp=min;
+	}else{
+		var min_e=$('#min li[data-sn='+min_tmp+']');
+		$(min_e).addClass('select');
+	}
+	$('#time_box').text(year_tmp+'-'+month_tmp+'-'+day_tmp+' '+hour_tmp+':'+min_tmp);
+}
+//
+function init (){
+	$('#date').after('<div id="box"><div id="time_box"></div><ul id="year"></ul><ul id="month"></ul><ul id="day"></ul><ul id="hour"></ul><ul id="min"></ul><ul id="action"><li data-action="ok">确定</li><li data-action="now">现在</li><li data-action="cancel">取消</li></ul></div>');
+	$('#box').show();
+	$('#box').css({"left":($("#datetime").width()-300)/2+10,"top":"10px"});
+	year_tmp	= undefined;
+	month_tmp	= undefined;
+	day_tmp		= undefined;
+	hour_tmp	= undefined;
+	min_tmp		= undefined;
+	//
+	var dtstr	= $('#date').text();
+	if (dtstr){
+		var now	= new Date(dtstr.replace(/-/g,"/"));
+	}else{
+		var now	= new Date();
+	}
+	year		= now.getFullYear();
+	month		= now.getMonth()+1;
+	day			= now.getDate();
+	day			= day<10?'0'+day:day;
+	hour		= now.getHours();
+	min			= now.getMinutes();
+	min			=min<10?'0'+min:min;
+	//
+	$('#year').html(year_content);
+	$('#month').html(month_content);
+	$('#day').html(day_content);
+	$('#hour').html(hour_content);
+	$('#min').html(min_content);
+	default_select();
+}
+//
+var current		= new Date();
+var year		= current.getFullYear();
+var month		= current.getMonth()+1;
+var day_number	= getDaysInMonth (year,month);
+var day			= current.getDate();
+var hour		= current.getHours();
+var min			= current.getMinutes();
+//
+var year_before	= parseInt(year)-2;
+var last_year	= parseInt(year)-1;
+var afya		= '<li data-sn="'+year_before+'">'+year_before+'</li>'+'<li data-sn="'+last_year+'">'+last_year+'</li>';
+var after_year	= '<li data-sn="'+year+'">'+year+'</li>';
+for(var i=1;i<=3;i++){
+	var after_year_tmp=parseInt(year)+parseInt(i);
+	after_year+=("<li data-sn="+after_year_tmp+">"+after_year_tmp+"</li>");
+}
+year_content=afya+after_year;
+
+//
+var month_content = '<li data-sn="1">1</li>';
+for(var i=2;i<=12;i++){
+	month_content+=("<li data-sn="+i+">"+i+"</li>");
+}
+//
+day_content = getdayscontent(day_number);
+//
+var hour_content = '';
+for(var i=0;i<=23;i++){
+	hour_content+=("<li data-sn="+i+">"+i+"</li>");
+}
+//
+var min_content	= '';
+for(var i=0;i<=59;i++){
+	i=i<10?'0'+i:i;
+	min_content+=("<li data-sn="+i+">"+i+"</li>");
+}
+var year_tmp;
+var month_tmp;
+var day_tmp;
+var hour_tmp;
+var min_tmp;
+
+$("body").on("click","#year li",function(){
+	$(this).addClass('select');
+	$('#year li').not($(this)).removeClass('select');
+	year_tmp = $(this).text();
+	day_content =getdayscontent(getDaysInMonth(year_tmp,month_tmp));
+	$('#day').html(day_content);
+	default_select();
+});
+$("body").on("click","#month li",function(){
+	$(this).addClass('select');
+	$('#month li').not($(this)).removeClass('select');
+	month_tmp = $(this).text();
+	day_content =getdayscontent(getDaysInMonth(year_tmp,month_tmp));
+	$('#day').html(day_content);
+	default_select();
+});
+$("body").on("click","#day li",function(){
+	$(this).addClass('select');
+	$('#day li').not($(this)).removeClass('select');
+	day_tmp = $(this).data('sn');
+	default_select();
+});
+$("body").on("click","#hour li",function(){
+	$(this).addClass('select');
+	$('#hour li').not($(this)).removeClass('select');
+	hour_tmp = $(this).data('sn');
+	default_select();
+});
+$("body").on("click","#min li",function(){
+	$(this).addClass('select');
+	$('#min li').not($(this)).removeClass('select');
+	min_tmp = $(this).data('sn');
+	default_select();
+});
+$("body").on("click","#action li",function(){
+	if ($(this).data('action')=='cancel'){
+		$('#box').hide();
+	}
+	if ($(this).data('action')=='now'){
+		var now		= new Date();
+		var year	= now.getFullYear();
+		var month	= now.getMonth()+1;
+		var day		= now.getDate();
+		day			= day<10?'0'+day:day;
+		var hour	= now.getHours();
+		var min		= now.getMinutes();
+		min			= min<10?'0'+min:min;
+		$('#date').text(year+'-'+month+'-'+day+' '+hour+':'+min);
+		$('#box').hide();
+		default_select();
+	}
+	if ($(this).data('action')=='ok'){
+		$('#date').text(year_tmp+'-'+month_tmp+'-'+day_tmp+' '+hour_tmp+':'+min_tmp);
+		$('#box').hide();
+	}
+});
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />
+	<title>Datetimer</title>
+	<meta name="generator" content="EverEdit" />
+	<meta name="author" content="" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta charset="utf-8" />
+	<meta content="yes" name="apple-mobile-web-app-capable">
+	<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0">
+	<link href="http://libs.baidu.com/bootstrap/3.0.3/css/bootstrap.css" rel="stylesheet">
+	<script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
+	<link href="datetime.css" rel="stylesheet">
+</head>
+<body>
+	<div id="datetime">
+	<span name="date" id="date"><?=date("Y-m-d H:i")?></span>
+</div>
+<script src="datetime.js"></script>
+<script>
+	$('#date').on('click',function(){
+		init();
+	});
+</script>
+</body>
+</html>
